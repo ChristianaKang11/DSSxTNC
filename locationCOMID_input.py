75a9a3bc5965a0a2678b43f32936c7b68645937c
ChristianaKang11 <christianakang@berkeley.edu>
2024-04-22 16:02:12 -0700

    geoencoder of textinput_unfinished

--- locationCOMID_input.py
+++ locationCOMID_input.py
@@ -1,26 +1,26 @@
 import streamlit as st
 import pandas as pd
-from geopy.geocoders import Nominatim
+# from geopy.geocoders import Nominatim
 
-def get_gages_by_(df, user_location):
-    def geocode_location(location):
-    geolocator = Nominatim(user_agent="streamlit_app")
-    location = geolocator.geocode(location)
-    if location:
-        return (location.latitude, location.longitude)
-    else:
-        return (None, None)
-    # Geocode the user location
-    lat, lon = geocode_location(user_location)
-    if lat is None or lon is None:
-        return None, "Unable to geocode the location."
+# def get_gages_by_(df, user_location):
+#     def geocode_location(location):
+#     geolocator = Nominatim(user_agent="streamlit_app")
+#     location = geolocator.geocode(location)
+#     if location:
+#         return (location.latitude, location.longitude)
+#     else:
+#         return (None, None)
+#     # Geocode the user location
+#     lat, lon = geocode_location(user_location)
+#     if lat is None or lon is None:
+#         return None, "Unable to geocode the location."
 
-    # Calculate distances from user location to all sites in the DataFrame
-    df['distance'] = ((df['latitude'] - lat)**2 + (df['longitude'] - lon)**2)**0.5
+#     # Calculate distances from user location to all sites in the DataFrame
+#     df['distance'] = ((df['latitude'] - lat)**2 + (df['longitude'] - lon)**2)**0.5
 
-    # Get the row with the smallest distance
-    nearest_site = df.loc[df['distance'].idxmin()]
-    return nearest_site['site_id'], nearest_site['sitename']
+#     # Get the row with the smallest distance
+#     nearest_site = df.loc[df['distance'].idxmin()]
+#     return nearest_site['site_id'], nearest_site['sitename']
 
 def get_gages_by_comid(df, comid):
 
